ea1da86e7899a730d5c5719383266f28d7ec39f1
lat9 <lat9@vinosdefrutastropicales.com>
2015-10-30 14:46:43 -0400

    Initial commit (v1.0.0)
    The initial GitHub commit for the plugin.

---
+++ includes/classes/observers/auto.prioritize_search_match_in_name.php
@@ -0,0 +1,53 @@
+<?php
+// -----
+// Part of the "Search: Prioritize Matching Names" plugin for Zen Cart v1.5.2 and later
+// Copyright (C) 2015, Vinos de Frutas Tropicales (lat9)
+// @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+//
+class zcObserverPrioritizeSearchMatchInName extends base {
+  public function __construct() {
+    $this->order_by = '';
+    $this->attach ($this, array ('NOTIFY_SEARCH_SELECT_STRING', 'NOTIFY_SEARCH_ORDERBY_STRING'));
+    
+  }
+
+  public function update (&$class, $eventID) {
+    switch ($eventID) {
+      case 'NOTIFY_SEARCH_SELECT_STRING': {
+        global $db, $keywords, $select_str;
+        if (isset ($keywords) && zen_not_null ($keywords) && zen_parse_search_string (stripslashes ($_GET['keyword']), $search_keywords)) {
+          $in_name_select = '';
+          foreach ($search_keywords as $current_keyword) {
+            switch ($current_keyword) {
+              case '(':
+              case ')':
+              case 'and':
+              case 'or': {
+                $in_name_select .= " $current_keyword ";
+                break;
+                
+              }
+              default: {
+                $in_name_select .= "pd.products_name LIKE '%:keywords%'";
+                $in_name_select = $db->bindVars ($in_name_select, ':keywords', $current_keyword, 'noquotestring');
+                break;
+              }
+            }
+          }
+          $select_str .= ", IF ($in_name_select, 1, 0) AS in_name ";
+          $this->order_by = ' in_name DESC,';
+          
+        }        
+        break;
+      }
+      case 'NOTIFY_SEARCH_ORDERBY_STRING': {
+        global $listing_sql;
+        $listing_sql = str_ireplace ('order by', 'order by' . $this->order_by, $listing_sql);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+}
