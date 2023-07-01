2b959a679897182e165dc776ad1e1c7bb110e501
lat9 <lat9@vinosdefrutastropicales.com>
2023-06-30 20:35:26 -0400

    #3: Define `order_by` property for PHP 7.4+

--- includes/classes/observers/auto.prioritize_search_match_in_name.php
+++ includes/classes/observers/auto.prioritize_search_match_in_name.php
@@ -1,24 +1,28 @@
 <?php
 // -----
-// Part of the "Search: Prioritize Matching Names" plugin for Zen Cart v1.5.3 and later
-// Copyright (C) 2015-2020, Vinos de Frutas Tropicales (lat9)
-// @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+// Part of the "Search: Prioritize Matching Names" plugin for Zen Cart v1.5.7 and later
+// Copyright (C) 2015-2023, Vinos de Frutas Tropicales (lat9)
 //
-class zcObserverPrioritizeSearchMatchInName extends base 
+// @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+//
+class zcObserverPrioritizeSearchMatchInName extends base
 {
-    public function __construct() 
+    protected
+        $order_by;
+
+    public function __construct()
     {
         $this->order_by = '';
         $this->attach(
-            $this, 
-            array(
-                'NOTIFY_SEARCH_SELECT_STRING', 
-                'NOTIFY_SEARCH_ORDERBY_STRING'
-            )
+            $this,
+            [
+                'NOTIFY_SEARCH_SELECT_STRING',
+                'NOTIFY_SEARCH_ORDERBY_STRING',
+            ]
         );
     }
 
-    public function update(&$class, $eventID) 
+    public function update(&$class, $eventID)
     {
         switch ($eventID) {
             case 'NOTIFY_SEARCH_SELECT_STRING':
@@ -42,7 +46,7 @@ class zcObserverPrioritizeSearchMatchInName extends base
                     }
                     $select_str .= ", IF ($in_name_select, 1, 0) AS in_name ";
                     $this->order_by = ' in_name DESC,';
-                }        
+                }
                 break;
 
             case 'NOTIFY_SEARCH_ORDERBY_STRING':
