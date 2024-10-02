6cde1d5712deac8069b4907793df37e2fe07fdf5
lat9 <lat9@vinosdefrutastropicales.com>
2024-10-02 12:44:57 -0400

    #4: Correct operation for zc200 and later
    Note: Drops support for Zen Cart versions prior to 2.00!

--- includes/classes/observers/auto.prioritize_search_match_in_name.php
+++ includes/classes/observers/auto.prioritize_search_match_in_name.php
@@ -1,9 +1,9 @@
 <?php
 // -----
-// Part of the "Search: Prioritize Matching Names" plugin for Zen Cart v1.5.8a and later
+// Part of the "Search: Prioritize Matching Names" plugin for Zen Cart v2.0.0 and later
 // Copyright (C) 2015-2024, Vinos de Frutas Tropicales (lat9)
 //
-// Last updated: v1.0.1
+// Last updated: v2.0.0
 //
 // @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
 //
@@ -16,62 +16,49 @@ class zcObserverPrioritizeSearchMatchInName extends base
         $this->attach(
             $this,
             [
-                'NOTIFY_SEARCH_WHERE_STRING',
-                'NOTIFY_SEARCH_ORDERBY_STRING',
+                'NOTIFY_SEARCH_SELECT_STRING',
+                'NOTIFY_SEARCH_REAL_ORDERBY_STRING',
             ]
         );
     }
 
-    public function update(&$class, $eventID)
+    public function update(&$class, $eventID, $p1, &$p2, &$p3)
     {
         switch ($eventID) {
-            case 'NOTIFY_SEARCH_WHERE_STRING':
-                global $search_keywords, $keywords, $select_str;
-                $in_name_select = '';
-                if (function_exists('zen_build_keyword_where_clause') && !empty($keywords)) {
-                    $in_name_select = zen_build_keyword_where_clause(['pd.products_name'], $keywords);
-                    $in_name_select = substr($in_name_select, 5);   //- Remove unwanted ' AND (' lead-in
-                } elseif (!empty($search_keywords)) {
-                    $in_name_select = $this->buildInNameSelectClause($search_keywords);
+            // -----
+            // From class.search.php
+            //
+            // $p1 ... (r/o) The current $select_str
+            // $p2 ... (r/w) The current $select_str
+            //
+            case 'NOTIFY_SEARCH_SELECT_STRING':
+                $keywords = $_GET['keyword'] ?? '';
+                if (empty($keywords)) {
+                    return;
                 }
-                if ($in_name_select !== '') {
-                    $select_str .= ", IF ($in_name_select, 1, 0) AS in_name ";
-                    $this->order_by = ' in_name DESC,';
+
+                $in_name_select = zen_build_keyword_where_clause(['pd.products_name'], $keywords);
+                $in_name_select = substr($in_name_select, 5);   //- Remove unwanted ' AND (' lead-in
+                 if ($in_name_select !== '') {
+                    $p2 .= ", IF ($in_name_select, 1, 0) AS in_name ";
+                    $this->order_by = 'in_name DESC, ';
                 }
                 break;
 
-            case 'NOTIFY_SEARCH_ORDERBY_STRING':
+            // -----
+            // From class.search.php
+            //
+            // $p1 ... (r/o) The current $order_str
+            // $p2 ... (r/w) The current $order_str
+            //
+            case 'NOTIFY_SEARCH_REAL_ORDERBY_STRING':
                 if (isset($this->order_by)) {
-                    global $listing_sql;
-                    $listing_sql = str_ireplace('order by', 'order by' . $this->order_by, $listing_sql);
+                    $p2 = str_ireplace('order by', 'ORDER BY ' . $this->order_by, $p2);
                 }
                 break;
 
             default:
                 break;
         }
-    }
-
-    protected function buildInNameSelectClause($search_keywords)
-    {
-        global $db;
-        $in_name_select = '';
-        foreach ($search_keywords as $current_keyword) {
-            switch ($current_keyword) {
-                case '(':
-                case ')':
-                    break;
-
-                case 'and':
-                case 'or':
-                    $in_name_select .= " $current_keyword ";
-                    break;
-
-                default:
-                    $in_name_select .= $db->bindVars("pd.products_name LIKE '%:keywords%'", ':keywords', $current_keyword, 'noquotestring');
-                    break;
-            }
-        }
-        return $in_name_select;
     }
 }
